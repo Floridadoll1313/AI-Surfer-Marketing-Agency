ee1de8ca99b897f4a1c4b52076234c2f9f786bcb
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-17 16:13:21 -0400

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home-page">
+      <h1>Welcome Home</h1>
+      <p>Your app is now wired correctly.</p>
+    </div>
+  );
+}
