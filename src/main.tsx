46bf543f4ba55e6678c81b8d3fb6aac347cb7e70
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-19 12:00:41 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
-// src/main.jsx
-import './styles/cinematic.css'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import PricingOverview from './pages/pricing'
 import ProductPage from './pages/pricing/[slug]'
 import ThankYou from './pages/thank-you'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/pricing" element={<PricingOverview />} />
-      <Route path="/pricing/:slug" element={<ProductPage />} />
-      <Route path="/thank-you" element={<ThankYou />} />
-    </Routes>
-  </BrowserRouter>
+// Import your global + cinematic styling
+import './index.css'
+import './styles/cinematic.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pricing" element={<PricingOverview />} />
+        <Route path="/pricing/:slug" element={<ProductPage />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
