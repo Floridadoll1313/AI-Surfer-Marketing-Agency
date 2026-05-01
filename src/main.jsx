da96b207f21093f620e683c74cf8a4ac79dc4165
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-05-01 12:51:39 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./index.css";
+
+import Landing from "./pages/Landing";
+import Services from "./pages/Services";
+import Members from "./pages/Members";
+import Contact from "./pages/Contact";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
