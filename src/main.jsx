a1754aa54c68e329974614b2207f00c97c5e2cd9
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-05-01 12:51:03 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from "./App";
 import './index.css';
 
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+
   <React.StrictMode>
     <BrowserRouter>
       <App />
