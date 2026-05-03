d54a4c2be54039536e4ab9366ecfb386bf5a12c3
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-05-03 18:20:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import cf from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // This config works for:
 // - Cloudflare Pages
